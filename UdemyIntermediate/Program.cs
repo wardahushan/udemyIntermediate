69dc14245cacbd2c4de23176073644d1d1d5f404
wardahushan <ushwar001@myuct.ac.za>
2018-10-05 23:19:07 +0200

    First attempt at solving StackOverflow Exercise

--- UdemyIntermediate/Program.cs
+++ UdemyIntermediate/Program.cs
@@ -11,9 +11,22 @@ namespace UdemyIntermediate
         static void Main(string[] args)
         {
             // RunStopWatchExercise(); 
+            // RunStackOverflowExercise();
+        }
 
+        private static void RunStackOverflowExercise()
+        {
+            Post post = new Post("Learning encapsulation");
+            post.Description = "This is my first exercise from Mosh's course learning how to apply the concepts of encapsulation.";
+            // post.Description = "Trying to update the description here.";
 
+            post.DownVote(); post.DownVote(); post.DownVote(); // 3 downvotes 
+            post.Upvote(); post.Upvote(); post.Upvote(); post.Upvote(); post.Upvote(); // 5 upvotes 
 
+            Console.WriteLine($"Post title: {post.Title}");
+            Console.WriteLine($"Post description: {post.Description}");
+            Console.WriteLine($"Post timestamp: {post.Timestamp}");
+            Console.WriteLine($"Number of votes: {post.GetVotes()}");
         }
 
         private static void RunStopWatchExercise()
