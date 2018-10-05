4277f8f4c1fc932ffddd55a700ee4fecf0718b81
wardahushan <ushwar001@myuct.ac.za>
2018-10-05 22:14:26 +0200

    Completed stopwatch exercise

---
+++ UdemyIntermediate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // RunStopWatchExercise(); 
+
+
+
+        }
+
+        private static void RunStopWatchExercise()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            System.Threading.Thread.Sleep(1000);
+            stopwatch.Stop();
+
+            stopwatch.Start();
+            System.Threading.Thread.Sleep(2000);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Elapsed time(s) are:");
+            foreach (TimeSpan duration in stopwatch.Durations())
+            {
+                Console.WriteLine(duration);
+            }
+        }
+    }
+}
---
+++ UdemyIntermediate/Stopwatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyIntermediate
+{
+    public class Stopwatch
+    {
+        private bool stopwatchStarted;
+        private DateTime startTime;
+        private List<TimeSpan> durations = new List<TimeSpan>();  
+
+        public void Start()
+        {
+            ValiidateStopwatchStart();
+            stopwatchStarted = true;
+            startTime = DateTime.Now; 
+        }
+
+        public void Stop()
+        {
+            ValiidateStopwatchStop();
+            stopwatchStarted = false;
+            durations.Add(DateTime.Now - startTime);
+        }
+
+        public List<TimeSpan> Durations()
+        {
+            return durations;
+        }
+
+        private void ValiidateStopwatchStop()
+        {
+            if (!stopwatchStarted)
+            {
+                throw new InvalidOperationException("Failed: stopwatch has not been started. Cannot execute method Stop() if stopwatch is not running.");
+            }
+        }
+
+        private void ValiidateStopwatchStart()
+        {
+            if (stopwatchStarted)
+            {
+                throw new InvalidOperationException("Failed: stopwatch has already been started. Cannot execute method Start() while stopwatch is running.");
+            }
+        }
+    }
+}
