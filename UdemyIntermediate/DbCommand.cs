2343afde7094d55a4fc5b90d301fba9f6a726802
wardahushan <ushwar001@myuct.ac.za>
2018-10-13 01:42:48 +0200

    Completed exercises for polymorphism

---
+++ UdemyIntermediate/DbCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyIntermediate
+{
+    public class DbCommand
+    {
+        private readonly DbConnection dbConnection;
+        private readonly string instruction;
+
+        public DbCommand(DbConnection dbConnection, string instruction)
+        {
+            if (string.IsNullOrWhiteSpace(instruction))
+            {
+                throw new NotSupportedException("Failed: DB command must be provided with an instruction.");
+            }
+
+            this.dbConnection = dbConnection;
+            this.instruction = instruction;
+        }
+
+        public void Execute()
+        {
+            dbConnection.OpenConnection();
+            Console.WriteLine("Running the instruction to the database... ");
+            dbConnection.CloseConnection();
+        }
+    }
+}
